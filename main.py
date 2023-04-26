06b3f8f836856c2e3a43f7a223b454e6f022a853
Mr. Lo <55058939+mrlo-codes@users.noreply.github.com>
2023-04-26 10:33:30 -0400

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-pins.digital_write_pin(DigitalPin.P0, 1)
+voltage = 0
 test = 0
+# This block sets the pin 0 to ON (1) to give power. To turn it off, set it to 0.
+# 
+# Note that the variables "test" and "voltage" had to be created in the Variables tab before using these blocks.
+pins.digital_write_pin(DigitalPin.P0, 1)
+# This block sets the value of the variable "test" to the value of the "voltage" read by the wire in pin 1. (From 0 to 1023, where 0 is no voltage to 1023, max voltage (around 3 V)).
+# 
+# The python code below prints out the value of test to the console. See the output by clicking on "Show data (Device)" on the left.
 
 def on_forever():
-    global test
-    test = pins.analog_read_pin(AnalogPin.P0)
+    global test, voltage
+    test = pins.analog_read_pin(AnalogPin.P1)
+    voltage = (1024 - test) / 1024 * 3.3
+    print("Analog read in: " + ("" + ("" + ("" + str(test)))) + " Voltage: " + ("" + ("" + ("" + str(voltage)))) + "V")
+    if voltage >= 3:
+        basic.show_string("L")
+    elif voltage >= 1:
+        basic.show_string("L")
+    else:
+        basic.show_number(0)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,16 +1,19 @@
-// This block sets the pin 0 to ON (1) to give power. To turn it off, set it to 0.
-pins.digitalWritePin(DigitalPin.P0, 1)
-// Note that the variable "test" had to be created in the Variables tab before using these blocks.
+let voltage = 0
 let test = 0
+// This block sets the pin 0 to ON (1) to give power. To turn it off, set it to 0.
+// 
+// Note that the variables "test" and "voltage" had to be created in the Variables tab before using these blocks.
+pins.digitalWritePin(DigitalPin.P0, 1)
+// This block sets the value of the variable "test" to the value of the "voltage" read by the wire in pin 1. (From 0 to 1023, where 0 is no voltage to 1023, max voltage (around 3 V)).
+// 
+// The python code below prints out the value of test to the console. See the output by clicking on "Show data (Device)" on the left.
 basic.forever(function () {
-    // This block sets the value of the variable "test" to the value of the "voltage" read by the wire in pin 1. (From 0 to 1023, where 0 is no voltage to 1023, max voltage (around 3 V)).
-    // 
-    // The python code below prints out the value of test to the console. See the output by clicking on "Show data (Device)" on the left.
     test = pins.analogReadPin(AnalogPin.P1)
-    console.log(test)
-if (test >= 500) {
+    voltage = (1024 - test) / 1024 * 3.3
+    console.log("Analog read in: " + ("" + ("" + ("" + test))) + " Voltage: " + ("" + ("" + ("" + voltage))) + "V")
+if (voltage >= 3) {
         basic.showString("H")
-    } else if (test >= 300) {
+    } else if (voltage >= 1) {
         basic.showString("L")
     } else {
         basic.showNumber(0)
